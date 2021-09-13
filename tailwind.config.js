2e49dc3c49297b313545f3704258cb0032c3b11b
yanyiik99 <aripramana574@gmail.com>
2021-09-13 10:38:58 +0800

    membuat navbar dan sudah responsive in index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,21 @@ module.exports = {
   purge: ['*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'logo': 'Jacques Francois',
+        'rightNav': 'Archivo Black',
+      },
+      minHeight: {
+        '115': '7.18rem',
+      },
+      colors: {
+        utama: "#323232",
+        oren: "#E3763E",
+        blueNavbar: "#2D4059",
+        navbarHover: "#1C3C65",
+      }
+    },
   },
   variants: {
     extend: {},
