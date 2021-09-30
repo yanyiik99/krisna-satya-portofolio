f7b4e97084d3cc672682f2e7b1c219cc713425ac
yanyiik99 <aripramana574@gmail.com>
2021-09-30 09:46:56 +0800

    create container, social media logo di index.html & make layer components in tailwind.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,11 @@ module.exports = {
     extend: {
       fontFamily: {
         'logo': 'Jacques Francois',
-        'rightNav': 'Archivo Black',
-	sans: ["Anton", "system-ui", "-apple- system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+        'rightNav': ['Archivo Black', 'sans'],
+	      'sans': ["Anton", "system-ui", "-apple- system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      },
+      fontSize: {
+        '110' : '6.87rem',
       },
       minHeight: {
         '115': '7.18rem',
@@ -17,7 +20,22 @@ module.exports = {
         oren: "#E3763E",
         blueNavbar: "#2D4059",
         navbarHover: "#1C3C65",
-      }
+      },
+      spacing: {
+        '216': '16.31rem',
+        '330': '20.62rem',
+        '350': '21.87rem',
+        '420': '26.25rem',
+        '430': '26.87rem',
+        '480': '30rem',
+        '500': '31.25rem',
+        '550': '34.37rem',
+        '580': '36.25rem',
+        '600': '37.5rem',
+        '680': '42.5rem',
+        '750': '46.87rem',
+        '800': '50rem',
+      },
     },
   },
   variants: {
